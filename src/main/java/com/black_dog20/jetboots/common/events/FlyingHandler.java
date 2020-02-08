f74916c7b863d99d96a4fc5d99f600a7996e5892
Crimix <christofferm@live.dk>
2020-02-08 17:50:52 +0100

    - Trying to fix problems with Angel ring (it failed)  - A least it works better than before

--- src/main/java/com/black_dog20/jetboots/common/events/FlyingHandler.java
+++ src/main/java/com/black_dog20/jetboots/common/events/FlyingHandler.java
@@ -2,6 +2,10 @@ package com.black_dog20.jetboots.common.events;
 
 import com.black_dog20.jetboots.Config;
 import com.black_dog20.jetboots.Jetboots;
+import com.black_dog20.jetboots.common.network.PacketHandler;
+import com.black_dog20.jetboots.common.network.packets.PacketSyncPartical;
+import com.black_dog20.jetboots.common.network.packets.PacketSyncSound;
+import com.black_dog20.jetboots.common.network.packets.PacketSyncStopSound;
 import com.black_dog20.jetboots.common.util.JetBootsProperties;
 import com.black_dog20.jetboots.common.util.JetbootsUtil;
 import com.black_dog20.jetboots.common.util.NBTTags;
@@ -22,61 +26,73 @@ import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerFlyableFallEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.PacketDistributor;
 
 @Mod.EventBusSubscriber(modid = Jetboots.MOD_ID)
 public class FlyingHandler {
 
-	@SubscribeEvent
+	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void onPlayerUpdate(TickEvent.PlayerTickEvent event) {
-		if(event.phase == TickEvent.Phase.END) { //END phase is important, otherwise this won't work because vanilla keeps resetting the elytra state!
-			if(event.player instanceof PlayerEntity) {
-				PlayerEntity player = event.player;
-				if(JetbootsUtil.canFlyWithBoots(player)) {
-					player.abilities.allowFlying = true;
-					ItemStack jetboots = JetbootsUtil.getJetBoots(player);
-					if(!player.getPersistentData().getBoolean(NBTTags.HAD_BOOTS_BEFORE)) {
-						player.getPersistentData().putBoolean(NBTTags.HAD_BOOTS_BEFORE, true);
-					}		
-					
-					if(JetBootsProperties.getEngineUpgrade(jetboots) && JetBootsProperties.getMode(jetboots) && (getAltitudeAboveGround(player) > 1.9 || player.isInWater())) {
-						drawpower(jetboots);
-						if(player.abilities.isFlying) {
-							player.getPersistentData().putBoolean(NBTTags.WAS_FLYING_BEFORE, true);
-							player.abilities.isFlying = false;
-						}
-					    player.setFlag(7, true);
-						Vec3d vec3d = player.getLookVec();
-						double d0 = 1.5D;
-						double d1 = 0.1D;
-						double d2 = 0.5D;
-						double d3 = 3D;
-						Vec3d vec3d1 = player.getMotion();
-						if(!player.isInWater()) {
-							double speed = JetBootsProperties.getSpeed(jetboots) ? d3 : d0;
-							player.setMotion(vec3d1.add(vec3d.x * d1 + (vec3d.x * speed - vec3d1.x) * d2, vec3d.y * d1 + (vec3d.y * speed - vec3d1.y) * d2, vec3d.z * d1 + (vec3d.z * speed - vec3d1.z) * d2));
+		PlayerEntity player = event.player;
+		if(JetbootsUtil.canFlyWithBoots(player)) {
+			player.abilities.allowFlying = true;  
+			ItemStack jetboots = JetbootsUtil.getJetBoots(player);
+			if(!player.getPersistentData().getBoolean(NBTTags.HAD_BOOTS_BEFORE)) {
+				player.getPersistentData().putBoolean(NBTTags.HAD_BOOTS_BEFORE, true);
+			}		
+
+			if(JetBootsProperties.getEngineUpgrade(jetboots) && JetBootsProperties.getMode(jetboots) && (getAltitudeAboveGround(player) > 1.9 || player.isInWater())) {
+				drawpower(jetboots);
+				if(player.abilities.isFlying) {
+					player.getPersistentData().putBoolean(NBTTags.WAS_FLYING_BEFORE, true);
+					player.abilities.isFlying = false;
+				}
+				player.setFlag(7, true);
+				Vec3d vec3d = player.getLookVec();
+				double d0 = 1.5D;
+				double d1 = 0.1D;
+				double d2 = 0.5D;
+				double d3 = 3D;
+				Vec3d vec3d1 = player.getMotion();
+				if(!player.isInWater()) {
+					double speed = JetBootsProperties.getSpeed(jetboots) ? d3 : d0;
+					player.setMotion(vec3d1.add(vec3d.x * d1 + (vec3d.x * speed - vec3d1.x) * d2, vec3d.y * d1 + (vec3d.y * speed - vec3d1.y) * d2, vec3d.z * d1 + (vec3d.z * speed - vec3d1.z) * d2));
+				} else {
+					double speed = JetBootsProperties.getSpeed(jetboots) ? d0 : d2;
+					player.setMotion(vec3d1.add(vec3d.x * d1 + (vec3d.x * speed - vec3d1.x) * d2, vec3d.y * d1 + (vec3d.y * speed - vec3d1.y) * d2, vec3d.z * d1 + (vec3d.z * speed - vec3d1.z) * d2));
+				}
+			} else if(player.isElytraFlying()) {
+				player.setFlag(7, false);
+				if(player.getPersistentData().getBoolean(NBTTags.WAS_FLYING_BEFORE)) {
+					player.abilities.isFlying = true;
+					player.sendPlayerAbilities();
+					player.getPersistentData().remove(NBTTags.WAS_FLYING_BEFORE);
+				}
+			} else if(player.abilities.isFlying) {
+				drawpower(jetboots);
+			}
+			if(event.side == LogicalSide.SERVER) {
+				if (JetbootsUtil.isFlying(player)) {
+					PacketHandler.NETWORK.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(()-> player), new PacketSyncPartical(player.getUniqueID(), player.abilities.isFlying));
+					if(!JetBootsProperties.getMuffledUpgrade(jetboots)) {
+						if (!player.isInWater()) {
+							PacketHandler.NETWORK.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(()-> player), new PacketSyncSound(player.getUniqueID()));
 						} else {
-							double speed = JetBootsProperties.getSpeed(jetboots) ? d0 : d2;
-							player.setMotion(vec3d1.add(vec3d.x * d1 + (vec3d.x * speed - vec3d1.x) * d2, vec3d.y * d1 + (vec3d.y * speed - vec3d1.y) * d2, vec3d.z * d1 + (vec3d.z * speed - vec3d1.z) * d2));
+							PacketHandler.NETWORK.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(()-> player), new PacketSyncStopSound(player.getUniqueID()));
 						}
-					} else if(player.isElytraFlying()) {
-					      player.setFlag(7, false);
-						if(player.getPersistentData().getBoolean(NBTTags.WAS_FLYING_BEFORE)) {
-							player.abilities.isFlying = true;
-							player.sendPlayerAbilities();
-							player.getPersistentData().remove(NBTTags.WAS_FLYING_BEFORE);
-						}
-					} else if(player.abilities.isFlying) {
-						drawpower(jetboots);
 					}
 				} else {
-					if(player.getPersistentData().getBoolean(NBTTags.HAD_BOOTS_BEFORE)) {
-						player.abilities.allowFlying = false;
-						player.abilities.isFlying = false;
-						player.getPersistentData().remove(NBTTags.HAD_BOOTS_BEFORE);
-					}
-				}
+					PacketHandler.NETWORK.send(PacketDistributor.TRACKING_ENTITY_AND_SELF.with(()-> player), new PacketSyncStopSound(player.getUniqueID()));
+				}}
+		} else {
+			if(player.getPersistentData().getBoolean(NBTTags.HAD_BOOTS_BEFORE)) {
+				player.abilities.allowFlying = false;
+				player.abilities.isFlying = false;
+				player.getPersistentData().remove(NBTTags.HAD_BOOTS_BEFORE);
 			}
 		}
 	}
