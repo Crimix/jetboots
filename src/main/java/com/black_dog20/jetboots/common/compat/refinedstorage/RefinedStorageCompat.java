24e7c39e20af078e37d24e96a8a2f928f4d70751
Crimix <christofferm@live.dk>
2020-12-27 12:25:47 +0100

    Removed unused code

--- src/main/java/com/black_dog20/jetboots/common/compat/refinedstorage/RefinedStorageCompat.java
+++ src/main/java/com/black_dog20/jetboots/common/compat/refinedstorage/RefinedStorageCompat.java
@@ -7,18 +7,13 @@ import com.black_dog20.jetboots.common.items.UpgradeItem;
 import com.black_dog20.jetboots.common.util.TranslationHelper;
 import com.refinedmods.refinedstorage.api.IRSAPI;
 import com.refinedmods.refinedstorage.api.RSAPIInject;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.world.World;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 
-import javax.annotation.Nullable;
-import java.util.List;
 import java.util.function.Function;
 
 import static com.black_dog20.jetboots.common.compat.refinedstorage.util.RefinedStorageNBTTags.*;
@@ -36,20 +31,8 @@ public class RefinedStorageCompat {
 
     public static void registerItems(DeferredRegister<Item> items) {
         if (ModList.get().isLoaded(MOD_ID)) {
-            WIRELESS_CRAFTING_UPGRADE = items.register("wireless_crafting_upgrade_rs", () -> new UpgradeItem(UpgradeItem.Type.HELMET, TranslationHelper.Compat.WIRELESS_CRAFTING_UPGRADE_INFO, createApplyUpgradeBoolean(TAG_HAS_WIRELESS_CRAFTING_UPGRADE), createValidateUpgradeBoolean(TAG_HAS_WIRELESS_CRAFTING_UPGRADE)) {
-                @Override
-                public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> tooltips, ITooltipFlag flag) {
-                    tooltips.add(info.get());
-                    //tooltips.add(WIRELESS_RANGE_UPGRADE_INFO_2.get());
-                }
-            });
-            WIRELESS_RANGE_UPGRADE = items.register("wireless_range_upgrade_rs", () -> new UpgradeItem(UpgradeItem.Type.HELMET, TranslationHelper.Compat.WIRELESS_RANGE_UPGRADE_INFO, createRangeUpgradeFunction(), createRangeUpgradeValidateFunction(), ModItems.ITEM_GROUP.maxStackSize(4)) {
-                @Override
-                public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> tooltips, ITooltipFlag flag) {
-                    tooltips.add(info.get());
-                    //tooltips.add(WIRELESS_RANGE_UPGRADE_INFO_2.get());
-                }
-            });
+            WIRELESS_CRAFTING_UPGRADE = items.register("wireless_crafting_upgrade_rs", () -> new UpgradeItem(UpgradeItem.Type.HELMET, TranslationHelper.Compat.WIRELESS_CRAFTING_UPGRADE_INFO, createApplyUpgradeBoolean(TAG_HAS_WIRELESS_CRAFTING_UPGRADE), createValidateUpgradeBoolean(TAG_HAS_WIRELESS_CRAFTING_UPGRADE)));
+            WIRELESS_RANGE_UPGRADE = items.register("wireless_range_upgrade_rs", () -> new UpgradeItem(UpgradeItem.Type.HELMET, TranslationHelper.Compat.WIRELESS_RANGE_UPGRADE_INFO, createRangeUpgradeFunction(), createRangeUpgradeValidateFunction(), ModItems.ITEM_GROUP.maxStackSize(4)));
         }
     }
 
